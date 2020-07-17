b543322d73b8203d419b5b120739cebbee24a12a
alanndz <alanmahmud0@gmail.com>
2020-07-17 15:24:24 +0800

    plugins: misc: add webreader.py
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

---
+++ userge/plugins/misc/webreader.py
@@ -0,0 +1,60 @@
+"""
+Scrape the article, translate and push to telegraph
+
+Author: alanndz <alanmahmud0@gmail.com>
+"""
+
+from newspaper import Article, ArticleException
+from telegraph import Telegraph
+from googletrans import Translator
+
+from userge import userge, Message
+
+
+@userge.on_cmd("wr", about={
+	'header': "Web Reader",
+	'usage': "{tr} [url] [*language]"})
+async def webreader(message: Message):
+	tgr = Telegraph()
+	tl = Translator()
+	inp = message.input_str
+
+	if len(inp.split()) > 1:
+		lang = inp.split()[1]
+	else: lang = None
+
+	url: str = inp.split()[0]
+	url: str = re.search(regex, url).group(1)
+
+	article: Article = Article(url)
+
+	try:
+		await message.edit("Getting text ...")
+		article.download()
+		article.parse()
+		text = f"{article.text}"
+	except ArticleException:
+		return await message.edit("Failed to scrape the article!")
+
+	if lang:
+		try:
+			await message.edit("Translating Text ...")
+			text = tl.translate(text, dest=lang)
+		except ValueError as err:
+			return await message.edit("Error: `{}`".format(str(err)))
+		text = text.text
+
+	await message.edit("Uploading to telegraph ...")
+	tgr.create_account(short_name='13378')
+	response = tgr.create_page(
+		f"{article.title}",
+		html_content="<p>{}</p>".format(text.replace("\n", "<br>"))
+	)
+
+	await message.edit(
+		"[Click Here](https://telegra.ph/{})".format(
+			response["path"]
+		)
+	)
+
+
